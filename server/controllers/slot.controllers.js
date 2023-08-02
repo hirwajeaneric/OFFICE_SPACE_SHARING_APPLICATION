d97d1a82a1dafb4e7368b7a555f7fc56621f8238
hirwajeaneric <hirwaminerve25@gmail.com>
2023-08-02 22:50:46 +0200

    Slot controller
    I added functionalities which allow the system to update office space slot numbers everytime a modification or a slot is added or deleted

--- server/controllers/slot.controllers.js
+++ server/controllers/slot.controllers.js
@@ -2,12 +2,15 @@ const SlotModel = require('../models/slot.model');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors/index');
 const multer= require('multer');
+const officeSpaceModel = require('../models/officeSpace.model');
 
 // Establishing a multer storage
 const multerStorage = multer.diskStorage({
     destination: (req, file, callback) => { callback(null, './spaces') },
     filename: (req, file, callback) => { callback(null, `space-${file.originalname}`) }
 })
+
+
 
 // Filter files with multer
 const multerFilter = (req, file, callback) => {
@@ -18,10 +21,14 @@ const multerFilter = (req, file, callback) => {
     }
   };
 
+
+
 const upload = multer({ 
     storage: multerStorage,
     fileFilter: multerFilter 
 });
+
+
 
 // Middleware for attaching files to the request body before saving.
 const attachFile = async (req, res, next) => {
@@ -62,18 +69,37 @@ const attachFile = async (req, res, next) => {
     next();
 }
 
+
+
 const add = async (req, res) => {
     const  slot = await SlotModel.create(req.body);
 
-    //Update office space number of slots and number of available slots
+    // Get existing office space
+    const existingOfficeSpace = await officeSpaceModel.findById(slot.spaceId);
 
-    res.status(StatusCodes.CREATED).json({ message: 'Successfully added', slot })
+    //Update office space number of slots and number of available slots
+    const updatedOfficeSpace = await officeSpaceModel.findByIdAndUpdate(
+        { 
+            id: existingOfficeSpace._id 
+        }, { 
+            numberOfSlots: existingOfficeSpace.numberOfSlots+1,
+            availableSlots: existingOfficeSpace.availableSlots+1 
+        }
+    );
+
+    if (updatedOfficeSpace) {
+        res.status(StatusCodes.CREATED).json({ message: 'Successfully added', slot });
+    }
 };
+
+
 
 const getAll = async(req, res) => {
     const  slots = await SlotModel.find({})
     res.status(StatusCodes.OK).json({ slots })
 };
+
+
 
 const findById = async(req, res) => {
     const  slotId = req.query.id;
@@ -84,11 +110,14 @@ const findById = async(req, res) => {
     res.status(StatusCodes.OK).json({ slot })
 };
 
+
+
 const findBySpaceId = async(req, res) => {
     const spaceId = req.query.spaceId;
     const slots = await SlotModel.find({ spaceId: spaceId });
     res.status(StatusCodes.OK).json({ slots });
 };
+
 
 
 const findByOccupantId = async(req, res) => {
@@ -97,35 +126,74 @@ const findByOccupantId = async(req, res) => {
     res.status(StatusCodes.OK).json({ slots });
 };
 
+
+
 const findByStatus = async(req, res) => {
     const status = req.query.status;
     const slots = await SlotModel.find({ status: status });
     res.status(StatusCodes.OK).json({ nbHits: slots.length, slots });
 };
 
+
+
 const edit = async(req, res) => {
     const  slot = req.body;
     const  slotId = req.query.id;
     
+    const previousSlot = await SlotModel.findById(slotId);
     const updated = await SlotModel.findByIdAndUpdate({ _id:  slotId }, slot);
     const updatedSlot = await SlotModel.findById(updated._id);
 
-    //Update office space number of slots and number of available slots
+    const existingOfficeSpace = await officeSpaceModel.findById(updatedSlot.spaceId);
 
-    res.status(StatusCodes.OK).json({ message: 'Updated', slot: updatedSlot })
+    // Change the number of slot according to the update
+    var slots = 0;
+    if (previousSlot === 'available' && (updatedSlot.status === 'booked' || updatedSlot.status === 'unavailable')) {
+        slots = existingOfficeSpace.availableSlots-1;
+    } else if ((previousSlot === 'unavailable' || previousSlot === 'booked') && updatedSlot.status === 'available') {
+        slots = existingOfficeSpace.availableSlots+1;
+    }
+
+    //Update office space number of slots and number of available slots
+    const updatedOfficeSpace = await officeSpaceModel.findByIdAndUpdate(
+        { 
+            id: existingOfficeSpace._id 
+        }, { 
+            availableSlots: slots 
+        }
+    );
+
+    if (updatedOfficeSpace) {
+        res.status(StatusCodes.OK).json({ message: 'Updated', slot: updatedSlot });
+    }
 };
+
+
 
 const remove = async(req, res) => {
     const slotId = req.query.id;
     const deletedSlot = await SlotModel.findByIdAndRemove({ _id: slotId});
 
-    //Update office space number of slots and number of available slots
-
     if (!deletedSlot) {
         throw new NotFoundError(`Slot with id ${slotId} not found!`);
     }
 
-    res.status(StatusCodes.OK).json({ message: 'Deleted'})
+    // Get existing office space
+    const existingOfficeSpace = await officeSpaceModel.findById(deletedSlot.spaceId);
+
+    //Update office space number of slots and number of available slots
+    const updatedOfficeSpace = await officeSpaceModel.findByIdAndUpdate(
+        { 
+            id: existingOfficeSpace._id 
+        }, { 
+            numberOfSlots: existingOfficeSpace.numberOfSlots-1,
+            availableSlots: existingOfficeSpace.availableSlots-1 
+        }
+    );
+
+    if (updatedOfficeSpace) {
+        res.status(StatusCodes.OK).json({ message: 'Deleted'})
+    }
 };
 
 module.exports = { add, getAll, findById, findByStatus, findByOccupantId, findBySpaceId, edit, upload, attachFile, remove }
