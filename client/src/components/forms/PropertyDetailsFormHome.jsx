4b9adeef8a5b394c3863698651d0861034af52fb
hirwajeaneric <hirwaminerve25@gmail.com>
2023-08-17 08:54:27 +0200

    Location

---
+++ client/src/components/forms/PropertyDetailsFormHome.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react'
+import { CustomFormControlOne, OfficeSpaceDetailsContainer, TopLeftFlexAlignedContainer } from '../styled-components/generalComponents'
+import { TextField, InputLabel, MenuItem, Select, Button } from '@mui/material';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { TypesOfOfficeSpaces } from '../../utils/TypesOfOfficeSpaces';
+import LocationMap from '../sections/LocationMap';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function PropertyDetailsFormHome(props) {
+  const { formData, setFormData } = props;
+  const params = useParams();
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    setUserData(JSON.parse(localStorage.getItem('usrInfo')));
+  },[params.officeSpaceId]);
+
+  const [picture, setPicture] = useState('');
+  
+  const [progress, setProgress] = useState({ value: '', disabled: false});
+  const [open, setOpen] = useState(false);
+  const [responseMessage, setResponseMessage] = useState({ message: '', severity: ''})
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const handleChangeOfficeSpaceType = (event) => {
+    setFormData({...formData, officeSpaceType: event.target.value});
+  };
+
+  const handleFileInput = (e) => {
+    setPicture(e.target.files[0]);
+  }
+
+  const handleChange = ({currentTarget: input}) => { 
+    setFormData({...formData, [input.name]: input.value}) 
+  };
+
+  const handleUpdateProperty = (e) => {
+    e.preventDefault();
+
+    var config = {};
+    var data = formData; 
+    delete data['_id'];
+    delete data['__v'];
+
+    if (!picture) {
+      config = {}
+    } else {
+      config = {
+        headers: { "Content-Type":"multipart/form-data" }
+      }
+      data.picture = picture;
+    }
+
+    setProgress({ value: 'Processing ...', disabled: true});
+
+    axios.put(`${process.env.REACT_APP_SERVERURL}/api/v1/ossa/officeSpace/update?id=${params.id}` , data, config)
+    .then(response => {
+      if (response.status === 200) {
+        setResponseMessage({ message: response.data.message, severity: 'success' });
+        setOpen(true);
+        setProgress({ value: '', disabled: false });
+        window.location.reload();
+      }
+    })
+    .catch(error => {
+      if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+        setResponseMessage({ message: error.response.data.msg, severity: 'error' });
+        setOpen(true);
+        setProgress({ value: '', disabled: false });
+      }
+    });
+  };
+
+  return (
+    <TopLeftFlexAlignedContainer onSubmit={handleUpdateProperty} style={{ flexDirection: 'column', width: '100%', justifyContent: 'flex-tart', gap: '20px', paddingBottom: '20px', border: '1px solid #d1e0e0', borderRadius: '5px', background: 'white' }}>
+      <OfficeSpaceDetailsContainer>
+        <div className="image-container" style={{ background: "url('"+process.env.REACT_APP_SERVERURL+"/api/v1/ossa/spaces/"+formData.picture+"')", backgroundOrigin: 'initial' }}>
+          <h3>{`Office space in ${formData.location}`}</h3>
+        </div>
+        <div className="space-details">
+          <div className="left">
+            <p>Description: <span>{formData.description}</span></p>
+            <p>Space type: <span>{formData.officeSpaceType}</span></p>
+            <p>Number of slots: <span>{formData.numberOfSlots}</span></p>
+            <p>Available slots: <span>{formData.availableSlots}</span></p>
+          </div>
+          <div className="right">
+            <p>Owner/Agent: <span>{formData.ownerName}</span></p>
+            <p>Phone of owner/agent: <span>{formData.ownerPhone}</span></p>
+            <p>Uploaded on: <span>{new Date(formData.lastUpdated).toDateString()}</span></p>
+          </div>
+        </div>
+      </OfficeSpaceDetailsContainer>        
+
+      {/* Response message Snackbar ****************************************************************************************  */}
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={responseMessage.severity} sx={{ width: '100%' }}>{responseMessage.message}</Alert>
+      </Snackbar>
+    </TopLeftFlexAlignedContainer>
+  )
+}
--- client/src/pages/PropertyDetailsHome.jsx
+++ client/src/pages/PropertyDetailsHome.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { useNavigate, useParams } from 'react-router-dom'
-import { FullWidthContainer, HeaderThree, HeaderTwo, PageSizedContainer, PropertyDescriptionSection } from '../components/styled-components/generalComponents';
+import { FullWidthContainer, HeaderThree, HeaderTwo, PageSizedContainer, PropertyDescriptionSection, SlotsContainer } from '../components/styled-components/generalComponents';
 import { PageWithSideBarContainer } from '../components/styled-components/generalComponents';
 import ImageSlider from '../components/sections/ImageCarousel';
 import PropertyMajorDetails from '../components/sections/PropertyMajorDetails';
@@ -10,21 +10,35 @@ import RentRequestForm from '../components/forms/RentRequestForm';
 import { Button } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOfficeSpaceDetails } from '../redux/features/officeSpaceSlice';
+import PropertyDetailsForm from '../components/forms/PropertyDetailsForm';
+import AddSlot from '../components/forms/AddSlot';
+import SlotsTable from '../components/tables/SlotsTable';
+import axios from 'axios';
+import { getSlotsForOfficeSpace } from '../redux/features/slotSlice';
+import PropertyDetailsFormHome from '../components/forms/PropertyDetailsFormHome';
 
 export default function PropertyDetailsHome() {
   const navigate = useNavigate();
   const params = useParams();
   const dispatch = useDispatch();
   const [user, setUser] = useState({});
+  const [formData, setFormData] = useState({ officeSpaceType: '', rentPrice: '', location: '', mapCoordinates: '', dimensions: '', description: '', bedRooms: '', bathRooms: '', furnished: false });
 
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem('usrInfo')));
-    
     dispatch(getOfficeSpaceDetails({officeSpaceId: params.id}));
+
+    axios.get(`${process.env.REACT_APP_SERVERURL}/api/v1/ossa/officeSpace/findById?id=${params.id}`)
+    .then(response => {
+      setFormData(response.data.officeSpace);
+      dispatch(getSlotsForOfficeSpace({ spaceId: params.id }))
+    })
+    .catch(error => console.log(error));
   },[dispatch, params.id]);
 
 
   const { selectedOfficeSpace, isLoading } = useSelector((state) => state.officeSpace);
+  const { slotsForOfficeSpace } = useSelector(state => state.slot);
 
   return (
     <FullWidthContainer>
@@ -32,80 +46,32 @@ export default function PropertyDetailsHome() {
         <title>Office space details</title>
         <meta name="description" content={`Details for office space.`} /> 
       </Helmet>
+
       <PageSizedContainer style={{ flexDirection: 'column', marginTop:'40px', padding: '0 10px' }}>
         {
           isLoading ? 
           <p style={{ margin:'40px 0' }}>Loading...</p>:
           <>
-          <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap', gap: '10px' }}>
-            <HeaderTwo style={{color: 'black', textAlign:'left'}}><strong>Office space for {selectedOfficeSpace.officeSpaceType} in {selectedOfficeSpace.location}</strong></HeaderTwo>
-          </div>
-          <PageWithSideBarContainer style={{ margin:'40px 0' }}>
-            <div className='leftSide'>
-              {/* <ImageSlider pictures={selectedOfficeSpace.picture} /> */}
-              <img src={`${process.env.REACT_APP_SERVERURL}/api/v1/ossa/spaces/${selectedOfficeSpace.picture}`} alt='' style={{ width: '100%'}}/>
-              <PropertyDescriptionSection>
-                <HeaderTwo>Description</HeaderTwo>
-                <p>
-                  {selectedOfficeSpace.description}
-                </p>
-              </PropertyDescriptionSection>
-              <PropertyMajorDetails descriptions={selectedOfficeSpace} />
-              <LocationMap coordinates={selectedOfficeSpace.mapCoordinates} />
-              
-              {/* Information about owner and tenant */}
-              <div style={{ border: '1px solid #d1e0e0', display:'flex', flexDirection: 'column', justifyContent:'flex-start', alignItems: 'flex-start', width: '100%', borderRadius: '5px', padding: '20px', background: 'white' }}>
-                {/* Owner info  */}
-                <div style={{ display:'flex', flexDirection: 'column', marginBottom: '20px', gap: '5px', justifyContent:'flex-start', alignItems: 'flex-start', width: '100%', }}>
-                  <HeaderThree style={{ marginBgottom: '10px' }}>Owner/Agent</HeaderThree>
-                  <p style={{ fontSize: '90%', color: 'gray', marginBottom: '5px' }}>Name: <br/><span style={{ color: 'black', fontSize: '100%' }}>{selectedOfficeSpace.ownerName}</span></p>
-                  <p style={{ fontSize: '90%', color: 'gray', marginBottom: '5px' }}>Phone: <br/><span style={{ color: 'black', fontSize: '100%' }}>{selectedOfficeSpace.ownerPhone}</span></p>
-                </div>
-                
-              </div>
-            </div>
-
-            {/* SIDE BAR WITH RENT AND JOIN FORM AND CALL TO ACTION MESSAGES ********************************************************** */}
+            {/* Property details  */}
+            <PropertyDetailsFormHome formData={formData} setFormData={setFormData}/>    
             
-            <div className='rightSide' style={{ border: '1px solid #d1e0e0', borderRadius: '5px', padding: '20px', background: 'white' }}>
-              
-              {/* This message appears when the selected house is owned by the user who has logen in */}
-              {/* {(user !== null && selectedOfficeSpace.ownerId === user.id) && <p>Your slot</p>} */}
-
-
-              {/* CALL TO ACTION MESSAGES  */}
-              {/* {user !== null && selectedOfficeSpace.ownerId !== user.id ?
-                <>
-                  <HeaderTwo>Do you want to Rent this slot?</HeaderTwo>
-                  <p style={{ fontWeight: '400', margin: '20px 0', lineHeight: '23px' }}>Fill in the form bellow to reserve the permission to rent this Apartment.</p>
-                </>
-                :
-                <></>  
-              } */}
-
-              {/* CONDITIONS TO DISPLAY RENT FORM */}
-              {/* {
-                !localStorage.getItem('usrTkn') && selectedOfficeSpace.status === 'For Rent' ? 
-                <Button 
-                  type='button' 
-                  variant='contained' 
-                  color='primary' 
-                  size='small' 
-                  style={{ marginBottom: '20px' }} 
-                  onClick={() => navigate('/signin')}>
-                    Sign in to Rent Apartment
-                </Button> : 
-                <></>
-              }
-
-              {
-                user !== null && selectedOfficeSpace.status === 'For Rent' && selectedOfficeSpace.ownerId !== user.id ? 
-                <RentRequestForm /> : 
-                <></>
-              } */}
+            <LocationMap coordinates={formData.mapCoordinates}/>
+            {/* Slots  */}
+            <SlotsContainer>
+              <div className='header-space'>
+                <h3>Slots</h3>
+              </div>
+              <AddSlot />
+              <SlotsTable data={slotsForOfficeSpace} />
+            </SlotsContainer>
+            <div style={{ display:'flex', flexDirection: 'column', marginBottom: '20px', gap: '5px', justifyContent:'flex-start', alignItems: 'flex-start', width: '100%', }}>
+              <HeaderThree style={{ marginBgottom: '10px' }}>Owner/Agent</HeaderThree>
+              <p style={{ fontSize: '90%', color: 'gray', marginBottom: '5px' }}>Name: <br/><span style={{ color: 'black', fontSize: '100%' }}>{selectedOfficeSpace.ownerName}</span></p>
+              <p style={{ fontSize: '90%', color: 'gray', marginBottom: '5px' }}>Phone: <br/><span style={{ color: 'black', fontSize: '100%' }}>{selectedOfficeSpace.ownerPhone}</span></p>
             </div>
-          </PageWithSideBarContainer>
-        </>}
+      
+          </>
+        }
       </PageSizedContainer>
     </FullWidthContainer>
   )
